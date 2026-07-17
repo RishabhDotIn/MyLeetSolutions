c8d30d7d2a5352ca79617efe596c4026716b4091
Rishabh Singh <89826015+RishabhDotIn@users.noreply.github.com>
2026-07-17 14:54:02 +0530

    Time: 2 ms (99.27%) | Memory: 46 MB (94.83%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,16 +1,28 @@
 class Solution {
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+               c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
     public String reverseVowels(String s) {
-        Set<Character> v=Set.of('A','E','I','O','U','a','e','i','o','u');
+        // Set<Character> v=Set.of('A','E','I','O','U','a','e','i','o','u');
         int i=0;
         int j=s.length()-1;
         char[] arr = s.toCharArray();
 
         while(i<j){
-            while (i < j && !v.contains(arr[i])) {
+            // while (i < j && !v.contains(arr[i])) {
+            //     i++;
+            // }
+
+            // while (i < j && !v.contains(arr[j])) {
+            //     j--;
+            // }
+            while (i < j && !isVowel(arr[i])) {
                 i++;
             }
 
-            while (i < j && !v.contains(arr[j])) {
+            while (i < j && !isVowel(arr[j])) {
                 j--;
             }
 
