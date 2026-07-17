ecb198183015451e1071bbbc525e4e83253c447a
Rishabh Singh <89826015+RishabhDotIn@users.noreply.github.com>
2026-07-17 12:09:58 +0530

    Time: 1 ms (70.65%) | Memory: 42.5 MB (23.75%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum+=i;
+        }
+        int prefix=0;
+        int i=1;
+        while(i<=n){
+            sum-=i;
+            if(sum==prefix)return i;
+            prefix+=i;
+            i++;
+        }
+        return -1;
+    }
+}
