3293b03355d1ef74543c76d1dbdb012bb86b2980
Rishabh Singh <89826015+RishabhDotIn@users.noreply.github.com>
2026-07-20 12:24:35 +0530

    Time: 4 ms (97.01%) | Memory: 47.5 MB (8.97%) - LeetSync

---
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -0,0 +1,59 @@
+class Solution {
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int arr[] = new int[m * n];
+        int j = 0;
+        for (int i = 0; i < m; i++) {
+            for (int z = 0; z < n; z++) {
+                arr[j] = grid[i][z];
+                j++;
+
+            }
+
+        }
+        int r = arr.length;
+        k = k % r;
+
+        reverse(arr, 0, r - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, r - 1);
+
+        j = 0;
+        for (int i = 0; i < m; i++) {
+            for (int z = 0; z < n; z++){
+                grid[i][z] = arr[j];
+                j++;
+                
+            }
+                
+
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < m; i++) {
+
+            List<Integer> l = new ArrayList<>();
+
+            for (int z = 0; z < n; z++) {
+                l.add(grid[i][z]);
+            }
+
+            ans.add(l);
+        }
+
+        return ans;
+
+    }
+
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
+    }
+}
