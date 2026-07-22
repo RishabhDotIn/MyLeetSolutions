10febe3f9142a9b9635ed27890bdb39f5924b310
Rishabh Singh <89826015+RishabhDotIn@users.noreply.github.com>
2026-07-22 12:24:28 +0530

    Time: 2 ms (97.98%) | Memory: 43.4 MB (84.01%) - LeetSync

---
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+
+        Map<Integer,Integer> map=new HashMap<>();
+        List<Integer> a=new ArrayList<>();
+
+        for(int n:arr){
+            map.put(n, map.getOrDefault(n, 0)+1);
+        }
+
+        for(int n:map.values()){
+            if(a.contains(n))return false;
+            a.add(n);
+        }
+        return true;
+        
+    }
+}
